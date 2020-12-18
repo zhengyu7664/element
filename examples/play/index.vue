6fd7d0c2766cad8f8ce91b086ce6f08a48b73147
zhengyu <474548186@qq.com>
2020-12-18 16:09:31 +0800

    add eu-table

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-04 10:40:03
- * @LastEditTime: 2020-12-07 14:43:36
+ * @LastEditTime: 2020-12-18 16:06:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /element/examples/play/index.vue
@@ -10,6 +10,45 @@
   <div style="margin: 20px;">
     <el-eu-button type="primary" @click="clickhandle">eu-button</el-eu-button>
     <el-eu-button type="danger" @click="clickhandle">eu-button</el-eu-button>
+
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    <el-divider></el-divider>
+    <el-eu-table
+      
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-eu-table>
   </div>
 </template>
 
@@ -17,7 +56,23 @@
   export default {
     data() {
       return {
-        input: 'Hello Element UI!'
+        tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
       };
     },
     methods:{
--- packages/eu-table/index.js
+++ packages/eu-table/index.js
@@ -1,4 +1,12 @@
-import EuTable from './src/main';
+/*
+ * @Author: your name
+ * @Date: 2020-12-16 16:20:52
+ * @LastEditTime: 2020-12-18 11:06:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \element\packages\eu-table\index.js
+ */
+import EuTable from './src/table';
 
 /* istanbul ignore next */
 EuTable.install = function(Vue) {
